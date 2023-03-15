7bacea7d6f71d12a4305e766446fb5a5635fd2e2
akinsuyitaiwo <teeboidayo@gmail.com>
2023-03-15 05:11:34 +0100

    Final Build

--- src/routes/event.js
+++ src/routes/event.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const {createEvent, viewEventById} = require('../controller/eventController.js');
+const {createEvent, viewEventById} = require('../controller/eventcontroller.js');
 const verifyToken = require('../middleware/auth.js');
 const router = Router();
 
